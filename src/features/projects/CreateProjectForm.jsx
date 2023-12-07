ad424e614e673736322ce4a0681042643addbae4
amir <orgamiryazdani@gmail.com>
2023-12-07 21:51:44 +0330

    add useOutsideClick and start create project form

---
+++ src/features/projects/CreateProjectForm.jsx
@@ -0,0 +1,11 @@
+import TextField from "../../ui/TextField"
+
+function CreateProjectForm() {
+    return (
+        <form className="">
+            <TextField />
+        </form>
+    )
+}
+
+export default CreateProjectForm
