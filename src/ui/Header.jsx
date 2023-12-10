d6b9b65fd3d70232b72609f43091062153f268a7
amir <orgamiryazdani@gmail.com>
2023-12-10 17:51:58 +0330

    Display the user's name in the header and add menus and complete the logout user section

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,18 @@
+import UserAvatar from "../features/authentication/UserAvatar"
 import useUser from "../features/authentication/useUser"
+import HeaderMenu from "./HeaderMenu"
 
 function Header() {
-    const { data } = useUser();
+    const { isLoading } = useUser();
+
     return (
-        <div className="bg-secondary-0 py-4 px-4">
-            app header
+        <div className="bg-secondary-0 py-4 px-4 border-b border-secondary-200">
+            <div className={`container xl:max-w-screen-lg flex items-center justify-end gap-x-8
+            ${isLoading ? "blur-sm opacity-50" : ""}
+            `}>
+                <UserAvatar />
+                <HeaderMenu />
+            </div>
         </div>
     )
 }
