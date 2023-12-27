50593613beb335f92680304b0cf1f8e1fb8652b7
amir <orgamiryazdani@gmail.com>
2023-12-27 13:05:50 +0330

    Start of admin panel development

---
+++ src/features/admin/AdminLayout.jsx
@@ -0,0 +1,31 @@
+import { HiCollection, HiHome, HiOutlineViewGrid, HiUser } from "react-icons/hi"
+import AppLayout from "../../ui/AppLayout"
+import CustomNavLink from "../../ui/CustomNavLink"
+import Sidebar from "../../ui/Sidebar"
+
+function AdminLayout() {
+    return (
+        <AppLayout>
+            <Sidebar>
+                <CustomNavLink to="dashboard">
+                    <HiHome />
+                    <span>داشبورد</span>
+                </CustomNavLink>
+                <CustomNavLink to="users">
+                    <HiUser />
+                    <span>کاربران</span>
+                </CustomNavLink>
+                <CustomNavLink to="projects">
+                    <HiOutlineViewGrid />
+                    <span>پروژه ها</span>
+                </CustomNavLink>
+                <CustomNavLink to="proposals">
+                    <HiCollection />
+                    <span>درخواست ها</span>
+                </CustomNavLink>
+            </Sidebar>
+        </AppLayout>
+    )
+}
+
+export default AdminLayout
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../features/admin/DashboardLayout"
+
+function AdminDashboard() {
+  return (
+    <DashboardLayout />
+  )
+}
+
+export default AdminDashboard
